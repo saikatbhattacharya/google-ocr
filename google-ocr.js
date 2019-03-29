5cf4c719e82ea86d8f0fea083d1cddbc39a5ab67
Saikat Bhattacharya <saikat.bhattacharya@nationstarmail.com>
2019-03-29 13:35:29 +0530

    Update google-ocr.js

--- google-ocr.js
+++ google-ocr.js
@@ -3,6 +3,8 @@ const axios = require('axios');
 const _ = require('lodash');
 const PDFImage = require('pdf-image').PDFImage;
 
+const image2base64 = require('image-to-base64');
+
 const url = "https://vision.googleapis.com/v1/images:annotate?key=AIzaSyATwGub6h3WxIja3LADI8V79F84_sry03k";
 const requestObj = {
   "requests": []
@@ -30,17 +32,27 @@ const pdf_to_image = async (filepath) => {
 }
 
 const base64_encode = (file) => {
-  let bitmap = fs.readFileSync(file);
-  return new Buffer(bitmap).toString('base64');
-  // return Buffer.from(bitmap, 'base64')
+  // let bitmap = fs.readFileSync(file);
+  // console.log(bitmap)
+  // return new Buffer(bitmap).toString('base64');
+  var request = require('request').defaults({ encoding: null });
+
+  request.get(file, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      data = new Buffer(body).toString('base64');
+      console.log(data);
+    }
+  });
 };
 
 const google_api = (fileArray) => {
   _.map(fileArray, each => {
-    const encodedFile = base64_encode(each);
+    // const encodedFile = base64_encode(each);
     const fileObj = {
       "image": {
-        "content": encodedFile
+        "source": {
+          "imageUri": each
+        }
       },
       "features": [{
         "type": "TEXT_DETECTION",
@@ -52,6 +64,7 @@ const google_api = (fileArray) => {
     };
     requestObj.requests.push(fileObj);
   });
+  console.log(JSON.stringify(requestObj))
   axios.post(url, requestObj)
     .then(outPut => {
       console.log(outPut.data.responses[0].textAnnotations[0].description);
@@ -59,8 +72,8 @@ const google_api = (fileArray) => {
         fs.writeFileSync('./output-' + i + '.txt', each.textAnnotations[0].description, 'utf8');
       })
     })
-    .catch(err => console.log(err.response))
+    .catch(err => console.log(err.responses))
 };
 
-google_api(['./test-0.png']);
-//pdf_to_image('./test.pdf');
+// pdf_to_image('./test.pdf');
+google_api(['test-0.png'])
