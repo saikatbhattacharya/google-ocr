44c7e8a9f4e5bb8a8110f3decebe6b70030dc0b8
Saikat Bhattacharya <saikat.bhattacharya@freshworks.com>
2019-04-05 16:53:32 +0530

    UI added

--- router.js
+++ router.js
@@ -4,23 +4,24 @@ var upload = multer({ dest: "./uploads" });
 var path = require('path');
 var cors = require('cors');
 var OCR = require('./requestHandler');
-
+var express = require('express');
 module.exports = function (app) {
   // parse application/x-www-form-urlencoded 
   app.use(bodyParser.urlencoded({ extended: false }))
+  app.use(express.static((path.join(__dirname, 'UI', 'build'))))
 
   // parse application/json 
   app.use(bodyParser.json())
   app.use(cors());
   app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname + '/index.html'));
+    res.sendFile(path.join(__dirname, 'UI', 'build', 'index.html'));
   })
 
   app.post("/fileupload", upload.single("filename"), function (req, res) {
     if (req.file.mimetype === 'application/pdf') OCR.process_pdf(req.file, res);
     else if (req.file.mimetype === 'image/jpeg' || req.file.mimetype === 'image/png') OCR.google_api(req.file, res);
     else {
-      res.sendStatus(400);
+      res.senFdStatus(400);
       res.send({ resData: 'File type is not supported' })
     }
   });
