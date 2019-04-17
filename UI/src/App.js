d0bae60822468e56642b98afe16d8f2ac5be8f6f
Souvik Sengupta <36044003+souvikcodefrags@users.noreply.github.com>
2019-04-17 18:17:02 +0530

    Added Lang Dropdown
    Added react-dropdown package

--- UI/src/App.js
+++ UI/src/App.js
@@ -1,53 +1,96 @@
-import React, { Component } from 'react';
-import { FilePond } from 'react-filepond';
-import 'filepond/dist/filepond.min.css';
-import './App.css';
-const uploadEndpoint = "/fileupload"
-//const uploadEndpoint = "http://ec2-18-188-52-63.us-east-2.compute.amazonaws.com:4000/fileupload"
-// const uploadEndpoint = "http://localhost:9000/fileupload"
+import React, { Component } from "react";
+import { FilePond } from "react-filepond";
+import Dropdown from "react-dropdown";
+import "filepond/dist/filepond.min.css";
+import "react-dropdown/style.css";
+import "./App.css";
+// const uploadEndpoint = "http://ec2-18-188-52-63.us-east-2.compute.amazonaws.com:4000/fileupload"
+const uploadEndpoint = "http://localhost:9000/fileupload";
+const langOptions = [
+  { value: null, label: "-Select a Language-" },
+  { value: "as", label: "Assamese" },
+  { value: "bn", label: "Bengali" },
+  { value: "en", label: "English" },
+  { value: "hi", label: "Hindi" },
+  { value: "mr", label: "Marathi" },
+  { value: "sa", label: "Sanskrit" },
+  { value: "ta", label: "Tamil" },
+  { value: "ur", label: "Urdu" }
+];
 
 class App extends Component {
-	constructor(props) {
-		super(props);
-		this.state = { responseText: [] }
-		this.handleResponse = this.handleResponse.bind(this);
-	}
-	handleResponse(newResponse) {
-		return (
-			this.state.responseText
-			+
-			<div>newResponse</div>
-		);
-	}
-	render() {
-		return (
-			<div className="App">
-				<header className="App-header">
-					<h1 className="App-title">Upload a pdf or jpg/png file</h1>
-					<h5>(PDF should not have more than 10 pages)</h5>
-				</header>
-				<div className="filepond-box">
-					<FilePond
-						allowMultiple={true}
-						server={uploadEndpoint}
-						name="filename"
-						onprocessfile={(error, file) => {
-							this.setState(
-								{
-									responseText: [
-										...this.state.responseText,
-										JSON.parse(file.serverId) ? JSON.parse(file.serverId).resData : 'Some error!!'
-									]
-								});
+  constructor(props) {
+    super(props);
+    this.state = { responseText: [], selectedOption: langOptions[0] };
+    this.handleResponse = this.handleResponse.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.handleFormData = this.handleFormData.bind(this);
+  }
+  handleResponse = (error, file) => {
+    if (!file.serverId) {
+      return null;
+    }
+    this.setState({
+      responseText: [
+        ...this.state.responseText,
+        JSON.parse(file.serverId).resData
+      ]
+    });
+  };
 
-						}} />
-				</div>
-				<div className="response-box">
-					{this.state.responseText.map((el, indx) => (<div className="response-box-item" key={indx}>{el}</div>))}
-				</div>
-			</div>
-		);
-	}
+  handleChange = selOpt => {
+    this.setState({ selectedOption: selOpt });
+  };
+
+  handleFormData = formData => {
+    formData.append("lang", this.state.selectedOption.value);
+    return formData;
+  };
+
+  render() {
+    const { selectedOption } = this.state;
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Upload File</h1>
+        </header>
+        <div className="lang-Dropdown">
+          <Dropdown
+            options={langOptions}
+            onChange={this.handleChange}
+            value={selectedOption}
+          />
+        </div>
+        {this.state.selectedOption.value !== null ? (
+          <div className="filepond-box">
+            <FilePond
+              server={{
+                url: uploadEndpoint,
+                timeout: 10000,
+                process: {
+                  method: "POST",
+                  onload: response => response,
+                  onerror: response => response.data,
+                  ondata: this.handleFormData
+                }
+              }}
+              allowMultiple={true}
+              name="filename"
+              lang={this.state.selectedOption.value}
+              onprocessfile={this.handleResponse}
+            />
+          </div>
+        ) : null}
+        <div className="response-box">
+          {this.state.responseText.map((el, indx) => (
+            <div className="response-box-item" key={indx}>
+              {el}
+            </div>
+          ))}
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
